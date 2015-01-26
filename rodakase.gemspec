e3728709b940d88b463819c1f44b21a0a17a768a
Piotr Solnica <piotr.solnica@gmail.com>
2015-01-26 23:32:42 +0100

    Add test suite setup

--- rodakase.gemspec
+++ rodakase.gemspec
@@ -22,4 +22,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "rspec", "~> 3.1"
+  spec.add_development_dependency "capybara"
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+# this is needed for guard to work, not sure why :(
+require "bundler"
+Bundler.setup
+
+if RUBY_ENGINE == "rbx"
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
+require 'rodakase'
+
+root = Pathname(__FILE__).dirname
+
+Dir[root.join('support/*.rb').to_s].each { |f| require f }
+Dir[root.join('shared/*.rb').to_s].each { |f| require f }
+
+RSpec.configure do |config|
+  config.before do
+    @constants = Object.constants
+  end
+
+  config.after do
+    added_constants = Object.constants - @constants
+    added_constants.each { |name| Object.send(:remove_const, name) }
+  end
+end
