898bdfb02715af365c3ecb1578467ed56759b2c3
Piotr Solnica <piotr.solnica@gmail.com>
2015-11-05 16:13:21 +0100

    Register rom container without the block

--- spec/dummy/core/boot/rom.rb
+++ spec/dummy/core/boot/rom.rb
@@ -2,23 +2,16 @@ require 'rom'
 require 'rom-repository'
 
 Dummy::Container.namespace('persistence') do |container|
-  container.register('rom') do
-    # TODO: extend container with memoization support
-    if ROM.container
-      ROM.container
-    else
-      ROM.use(:auto_registration)
+  ROM.use(:auto_registration)
 
-      ROM.setup(:sql, container.config.app.database_url)
+  ROM.setup(:sql, container.config.app.database_url)
 
-      %w(relations commands).each do |type|
-        Dir[container.root.join("lib/persistence/#{type}/**/*.rb")]
-          .each(&method(:require))
-      end
-
-      ROM.finalize.container
-    end
+  %w(relations commands).each do |type|
+    Dir[container.root.join("lib/persistence/#{type}/**/*.rb")]
+      .each(&method(:require))
   end
+
+  container.register('rom', ROM.finalize.container)
 
   container.auto_load!(container.root.join('lib/persistence/repositories')) do |repo_class|
     -> { Inflecto.constantize(repo_class).new(container['persistence.rom']) }
