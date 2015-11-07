a823183e694d93a94733c8c4ccd85358342ba5dc
Piotr Solnica <piotr.solnica@gmail.com>
2015-11-07 17:06:44 +0100

    Register logger and renderer as singletons

--- spec/dummy/core/boot.rb
+++ spec/dummy/core/boot.rb
@@ -1,16 +1,15 @@
-require 'rodakase/view/renderer'
+require 'logger'
+
+require 'rodakase/view'
+require 'rodakase/transaction'
 
 require_relative 'dummy/application'
 require_relative 'dummy/view'
 
 Dummy::Container.configure do |container|
-  container.register(:logger) do
-    Logger.new(container.root.join('log/app.log'))
-  end
+  container.register(:logger, Logger.new(container.root.join('log/app.log')))
 
-  container.register(:renderer) do
-    Rodakase::View::Renderer.new(container.root.join('templates'))
-  end
+  container.register(:renderer, Rodakase::View::Renderer.new(container.root.join('templates')))
 end
 
 Dummy::Container.finalize!
