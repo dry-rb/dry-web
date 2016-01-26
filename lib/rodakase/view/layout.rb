07af119cba771053070412b7b33a52c0fc781c7e
Tim Riley <tim@openmonkey.com>
2016-01-26 17:03:14 +1100

    Introduce View::RenderContext to render a view with a specified format
    This also allows the parts for a view to be accessed with a specified format in the same way.

--- lib/rodakase/view/layout.rb
+++ lib/rodakase/view/layout.rb
@@ -1,16 +1,12 @@
 require 'dry-configurable'
 require 'dry-equalizer'
 
-require 'rodakase/view/part'
-require 'rodakase/view/null_part'
-require 'rodakase/view/renderer'
+require 'rodakase/view/render_context'
 
 module Rodakase
   module View
     class Layout
       include Dry::Equalizer(:config)
-
-      Scope = Struct.new(:page)
 
       DEFAULT_SCOPE = Object.new.freeze
       DEFAULT_DIR = 'layouts'.freeze
@@ -21,7 +17,7 @@ module Rodakase
       setting :root
       setting :name
       setting :template
-      setting :default_format, 'html'
+      setting :format, 'html'
       setting :scope
 
       attr_reader :config, :scope,
@@ -36,57 +32,28 @@ module Rodakase
       end
 
       def call(options = {})
-        renderer = Renderer.new(config.root, format: options.fetch(:format, config.default_format), engine: config.engine)
-
-        renderer.(layout_path, layout_scope(options, renderer)) do
-          renderer.(template_path, template_scope(options, renderer))
-        end
+        render_context(options).render
       end
 
-      def layout_scope(options, renderer)
-        Scope.new(layout_part(:page, options.fetch(:scope, scope), renderer))
-      end
+      def parts(locals, options = {})
+        return DEFAULT_SCOPE unless locals.any?
 
-      def template_scope(options, renderer)
-        parts(locals(options), renderer)
+        render_context(options).parts(locals)
       end
 
       def locals(options)
         options.fetch(:locals, {})
       end
 
-      def parts(locals, renderer)
-        return DEFAULT_SCOPE unless locals.any?
+      private
 
-        part_hash = locals.each_with_object({}) do |(key, value), result|
-          part =
-            case value
-            when Array
-              el_key = Inflecto.singularize(key).to_sym
-              template_part(key, value.map { |element| template_part(el_key, element, renderer) }, renderer)
-            else
-              template_part(key, value, renderer)
-            end
-
-          result[key] = part
-        end
-
-        part(template_path, part_hash, renderer)
-      end
-
-      def layout_part(name, value, renderer)
-        part(layout_dir, {name => value}, renderer)
-      end
-
-      def template_part(name, value, renderer)
-        part(template_path, {name => value}, renderer)
-      end
-
-      def part(dir, value, renderer)
-        value_present = value.values.first
-        part_class = value_present ? Part : NullPart
-
-        part_class.new(renderer.chdir(dir), value)
+      def render_context(options)
+        RenderContext.new(
+          self,
+          options.fetch(:format, config.format),
+          options.fetch(:engine, config.engine),
+          options
+        )
       end
     end
   end
