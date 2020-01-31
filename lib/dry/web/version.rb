d7993872a40b524a2f0bf9c2d361962577a47742
Piotr Solnica <piotr.solnica@gmail.com>
2020-01-31 09:00:43 +0100

    rubocop -a --only Style/FrozenStringLiteralComment

--- lib/dry/web/version.rb
+++ lib/dry/web/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Dry
   module Web
     VERSION = '0.8.0'.freeze
--- spec/unit/container_spec.rb
+++ spec/unit/container_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rack'
 require 'dry/monitor'
 require 'dry/monitor/rack/middleware'
